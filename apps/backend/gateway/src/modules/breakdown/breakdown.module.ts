bfd59bfe15f1d322450273b72c51b3b6eca31676
Will Beaumont <will@wjbeaumont.org>
2020-12-16 11:41:58 -0800

    Added missing import

--- apps/backend/gateway/src/modules/breakdown/breakdown.module.ts
+++ apps/backend/gateway/src/modules/breakdown/breakdown.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BreakdownModel, BreakdownSchema } from '../../schema/breakdown.schema';
 import { ProjectModel, ProjectSchema } from '../../schema/project.schema';
 import { BreakdownService } from './breakdown.service';
+import { ProjectModule } from '../project/project.module';
 
 @Module({
     imports: [
+        ProjectModule,
         MongooseModule.forFeature([
             { name: BreakdownModel.name, schema: BreakdownSchema },
             { name: ProjectModel.name, schema: ProjectSchema }
